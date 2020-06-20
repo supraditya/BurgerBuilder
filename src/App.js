ca3c648f0b819e7f71eb880b7b7a47e5d6678bb1
[supraditya] <[aditya300100@gmail.com]>
2020-06-21 02:34:18 +0530

    Layout wrapper component, higher order component and font styling added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, {Component} from 'react';
-import './App.css';
-
-class App extends Component{
-  render()
-  {
+import React, { Component } from "react";
+import Layout from './components/Layout/Layout';
+class App extends Component {
+  render() {
     return (
       <div className="App">
-        <h1>App works</h1>
+        <Layout>
+            <p>Test</p>
+        </Layout>
       </div>
     );
   }
-  
 }
 
 export default App;
