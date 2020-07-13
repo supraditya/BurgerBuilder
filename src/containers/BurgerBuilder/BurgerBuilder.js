ffd5de9303eb0617856f2008cd174b978fdf2c07
[supraditya] <[aditya300100@gmail.com]>
2020-07-13 18:02:36 +0530

    Added burger components stylesheet, functional changes

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import Auxiliary from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component{
+    state={
+        ingredients:{
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
     render()
     {
         return(
             <Auxiliary>
                 <div>
-                    Burger representation
+                    <Burger ingredients={this.state.ingredients}/>
                 </div>
                 <div>
                     Build controls
